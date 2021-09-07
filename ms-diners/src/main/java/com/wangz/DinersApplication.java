e46a85e544783c1160fbf3677284edb04970b897
wangzbest <wangzbest@163.com>
2021-09-07 23:17:04 +0800

    项目架构与微服务搭建

---
+++ ms-diners/src/main/java/com/wangz/DinersApplication.java
@@ -0,0 +1,13 @@
+package com.wangz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinersApplication.class, args);
+    }
+
+}
