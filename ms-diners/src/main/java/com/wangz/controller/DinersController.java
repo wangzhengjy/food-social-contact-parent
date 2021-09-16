37679e7339fc9dc184b297476e7978c205f3904e
wangzbest <wangzbest@163.com>
2021-09-16 21:34:59 +0800

    食客服务登录业务完善

---
+++ ms-diners/src/main/java/com/wangz/controller/DinersController.java
@@ -0,0 +1,64 @@
+package com.wangz.controller;
+
+import com.wangz.model.domain.ResultInfo;
+import com.wangz.model.dto.DinersDTO;
+import com.wangz.utils.ResultInfoUtil;
+import com.wangz.service.DinersService;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 食客服务控制层
+ */
+@RestController
+@Api(tags = "食客相关接口")
+public class DinersController {
+
+    @Resource
+    private DinersService dinersService;
+
+    @Resource
+    private HttpServletRequest request;
+
+    /**
+     * 注册
+     *
+     * @param dinersDTO
+     * @return
+     */
+//    @PostMapping("register")
+//    public ResultInfo register(@RequestBody DinersDTO dinersDTO) {
+//        return dinersService.register(dinersDTO, request.getServletPath());
+//    }
+
+    /**
+     * 校验手机号是否已注册
+     *
+     * @param phone
+     * @return
+     */
+//    @GetMapping("checkPhone")
+//    public ResultInfo checkPhone(String phone) {
+//        dinersService.checkPhoneIsRegistered(phone);
+//        return ResultInfoUtil.buildSuccess(request.getServletPath());
+//    }
+
+    /**
+     * 登录
+     *
+     * @param account
+     * @param password
+     * @return
+     */
+    @GetMapping("signin")
+    public ResultInfo signIn(String account, String password) {
+        return dinersService.signIn(account, password, request.getServletPath());
+    }
+
+}
