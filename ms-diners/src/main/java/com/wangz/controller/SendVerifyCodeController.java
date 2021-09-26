cf743489c1901ae1b907fe13f70c9634dcb7e9fd
wangzbest <wangzbest@163.com>
2021-09-26 22:20:13 +0800

    Redis保存手机短信验证码

---
+++ ms-diners/src/main/java/com/wangz/controller/SendVerifyCodeController.java
@@ -0,0 +1,38 @@
+package com.wangz.controller;
+
+import com.wangz.model.domain.ResultInfo;
+import com.wangz.service.SendVerifyCodeService;
+import com.wangz.utils.ResultInfoUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 发送验证码控制层
+ */
+@RestController
+public class SendVerifyCodeController {
+
+    @Resource
+    private SendVerifyCodeService sendVerifyCodeService;
+
+    @Resource
+    private HttpServletRequest request;
+
+    /**
+     * 发送验证码
+     *
+     * @param phone
+     * @return
+     */
+    @GetMapping("send")
+    public ResultInfo send(String phone) {
+        sendVerifyCodeService.send(phone);
+        return ResultInfoUtil.buildSuccess("发送成功", request.getServletPath());
+    }
+
+
+}
+
