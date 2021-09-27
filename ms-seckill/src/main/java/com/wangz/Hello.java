e26a139c8d3d3a4f51a97468fde1fb367c7cb93d
wangzbest <wangzbest@163.com>
2021-09-27 23:18:39 +0800

    新增购买代金券的活动和购买代金券 仅利用关系型数据库 ，未用redis和秒杀业务

---
+++ ms-seckill/src/main/java/com/wangz/Hello.java
@@ -0,0 +1,4 @@
+package com.wangz;
+
+public class Hello {
+}
---
+++ ms-seckill/src/main/java/com/wangz/controller/SeckillController.java
@@ -0,0 +1,51 @@
+package com.wangz.controller;
+
+import com.wangz.model.domain.ResultInfo;
+import com.wangz.model.pojo.SeckillVouchers;
+import com.wangz.utils.ResultInfoUtil;
+import com.wangz.service.SeckillService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 秒杀控制层
+ */
+@RestController
+public class SeckillController {
+
+    @Resource
+    private SeckillService seckillService;
+    @Resource
+    private HttpServletRequest request;
+    /**
+     * 秒杀下单
+     *
+     * @param voucherId
+     * @param access_token
+     * @return
+     */
+    @PostMapping("{voucherId}")
+    public ResultInfo<String> doSeckill(@PathVariable Integer voucherId, String access_token) {
+        ResultInfo resultInfo = seckillService.doSeckill(voucherId, access_token, request.getServletPath());
+        return resultInfo;
+    }
+
+    /**
+     * 新增秒杀活动
+     *
+     * @param seckillVouchers
+     * @return
+     */
+    @PostMapping("add")
+    public ResultInfo<String> addSeckillVouchers(@RequestBody SeckillVouchers seckillVouchers) {
+        seckillService.addSeckillVouchers(seckillVouchers);
+        return ResultInfoUtil.buildSuccess(request.getServletPath(),
+                "添加成功");
+    }
+
+}
